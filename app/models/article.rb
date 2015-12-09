13805606064d63c43f63c6d7573deb84c66105b7
Mike Nacke Alpha Blog <mike@nackemedia.com>
2015-12-09 16:40:05 +0000

    added associations between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: { mimimum: 3, maximum: 50 }
     validates :description, presence: true, length: { mimimum: 10, maximum: 300 }
-    
+    validates :user_id, presence: true
 end
